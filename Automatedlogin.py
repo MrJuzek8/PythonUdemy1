02376de418c319c941597aa539d07d294cfc07bc
MrJuzek8 <122978885+MrJuzek8@users.noreply.github.com>
2024-09-06 10:09:53 +0000

    Loging and scraping

--- Automatedlogin.py
+++ Automatedlogin.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 def get_driver():
@@ -10,20 +11,27 @@ def get_driver():
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
-  
+
   driver = webdriver.Chrome(options=options)
-  driver.get("https://automated.pythonanywhere.com/")
+  driver.get("https://automated.pythonanywhere.com/login/")
   return driver
+  
 def clean_text(text):
   """Extract only temp from text"""
   output = float(text.split(": ")[1])
   return output
 
-
 def main():
   driver = get_driver()
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2) 
+  driver.find_element(by="id",value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
   time.sleep(2)
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)
+  text = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+  return clean_text(text)
+  
+                        
+  
 
 print(main())
